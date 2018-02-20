7058077419bde7cce0ee17405f2aeb5c71f5e932
Osher E <osher.filter@gmail.com>
2018-02-20 14:06:38 +0200

    savepoint

--- quizes.GET/index.js
+++ quizes.GET/index.js
@@ -1,7 +1,12 @@
 'use strict';
 
-exports.http = (request, response) => {
-  response.status(200).send('Hello World!');
+const mock = require('./mocks/client-view.json');
+const mockView = JSON.stringify(mock);
+exports.quizes = (request, response) => {
+  response.status(200);
+  response.setHeader('Content-Type', 'application/json');
+  response.setHeader('x-foo', 'bar');
+  response.send(mockView);
 };
 
 exports.event = (event, callback) => {
