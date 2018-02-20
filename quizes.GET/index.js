80adc5071491f0f6c25c72b72059ecd5c6dd1777
Osher E <osher.filter@gmail.com>
2018-02-20 14:21:45 +0200

    model data

--- quizes.GET/index.js
+++ quizes.GET/index.js
@@ -1,11 +1,18 @@
 'use strict';
 
+const cors = r => {
+    r.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, HEAD');
+    r.setHeader('Access-Control-Allow-Origin', '*');
+    r.setHeader('Access-Control-Max-Age', '3000');
+};
 const mock = require('./mocks/client-view.json');
 const mockView = JSON.stringify(mock);
 exports.quizes = (request, response) => {
   response.status(200);
+  cors(response);
   response.setHeader('Content-Type', 'application/json');
   response.setHeader('x-foo', 'bar');
+  
   response.send(mockView);
 };
 
